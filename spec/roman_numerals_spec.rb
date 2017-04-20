e6eca6628c39bb6fac09572ce4702d61e8d8d43d
Jason Fleagle <jasonfleagle@hotmail.com>
2017-04-20 19:31:47 -0400

    Failing test for 5

--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -4,4 +4,9 @@ describe RomanNumerals do
   it "should be able to convert 1 to I" do
     expect(RomanNumerals.convert(1)).to eq("I")
   end
+
+  it "should be able to convert 5 to V" do
+    expect(RomanNumerals.convert(5)).to eq("V")
+  end
+
 end
